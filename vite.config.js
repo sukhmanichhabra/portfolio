9c64cbb0a89b6a3df80175eb1054a187099d453f
sukhmani chhabra <149159326+sukhmanichhabra@users.noreply.github.com>
2024-10-28 19:41:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
-  return {
-    define: {
-      __APP_ENV__: process.env.VITE_VERCEL_ENV,
-    },
-    plugins: [react()]
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+plugins: [react()],
+})
